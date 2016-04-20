4871d7d113e8838ce227bf483cc09de590af4587
David Ainslie <dainslie@gmail.com>
2016-04-20 20:10:07 +0100

    Can run assertions a set number of times

---
+++ src/test/scala/uk/gov/homeoffice/specs2/package.scala
@@ -0,0 +1,34 @@
+package uk.gov.homeoffice
+
+import org.specs2.matcher.MatchResult
+
+package object specs2 {
+  /**
+    * Run a given match (assertion) twice.
+    * @param r MatchResult[R]
+    * @tparam R Type of match
+    * @return MatchResult[R]
+    */
+  def twice[R](r: => MatchResult[R]): MatchResult[R] = times(2)(r)
+
+  /**
+    * Run a given match (assertion) a number of times.
+    * @param n Int Number of times to run the match (assertion).
+    * @param r MatchResult[R]
+    * @tparam R Type of match
+    * @return MatchResult[R]
+    */
+  def times[R](n: Int)(r: => MatchResult[R]): MatchResult[R] = {
+    def run(count: Int, result: MatchResult[R]): MatchResult[R] = {
+      if (count > 1) {
+        if (result.isSuccess) {
+          run(count - 1, r)
+        }
+      }
+
+      result
+    }
+
+    run(n, r)
+  }
+}
