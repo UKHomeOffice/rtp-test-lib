1f31f2297bb54d78473f94cdf08757643cf795c2
David Ainslie <dainslie@gmail.com>
2016-05-03 14:19:57 +0100

    Reverting Specs2 upgrade

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,7 @@ object Build extends Build {
         "Kamon Repository" at "http://repo.kamon.io"
       ),
       libraryDependencies ++= {
-        val `specs2-version` = "3.8"
+        val `specs2-version` = "3.7.3"
 
         Seq(
           "org.scalactic" %% "scalactic" % "2.2.6" withSources(),
