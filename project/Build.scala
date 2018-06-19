89343d19963e54007432a8d0b51ac4e5b86695e0
Phillip Taylor <phillip.taylor@transformuk.com>
2018-06-19 16:12:57 +0100

    DPS-1410: Cross-compile and build for both scala 2.11 and 2.12

--- project/Build.scala
+++ project/Build.scala
@@ -8,26 +8,8 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      scalaVersion := "2.11.8",
-      scalacOptions ++= Seq(
-        "-feature",
-        "-language:implicitConversions",
-        "-language:higherKinds",
-        "-language:existentials",
-        "-language:reflectiveCalls",
-        "-language:postfixOps",
-        "-Yrangepos",
-        "-Yrepl-sync"
-      ),
-      ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
-      resolvers ++= Seq(
-        "Artifactory Snapshot Realm" at "http://artifactory.registered-traveller.homeoffice.gov.uk/artifactory/libs-snapshot-local/",
-        "Artifactory Release Realm" at "http://artifactory.registered-traveller.homeoffice.gov.uk/artifactory/libs-release-local/",
-        "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-        "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
-        "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases",
-        "Kamon Repository" at "http://repo.kamon.io"
-      ),
+      scalaVersion := "2.12.6",
+      crossScalaVersions := Seq("2.11.8", "2.12.6"),
       libraryDependencies ++= {
         val `specs2-version` = "3.8.9"
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,1 @@
-resolvers += Classpaths.sbtPluginReleases
-
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
